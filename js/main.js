0ce33c1eefb19dc12d6a439ef8dc95d8f8a20e99
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-03-10 18:21:23 -0500

    creacion de la coleccion de objetos, y funcion compose para la creacion de las notificaciones y sus diferentes casos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+// Notifications collection
+
+/*
+
+
+ - avatar: string,
+ - name: string,
+ - content: string,
+ - date: string,
+ - unread: boolean,
+ - type: string (standard | link | message | picture)
+ - meta?: object
+        linkType?: string (post | group)
+        linkText?: string
+        messge?: string
+        picture?: string
+*/
+
+const notificationCollection =[
+    {
+        avatar:'assets/images/avatar-mark-webber.webp',
+        name: 'Mark Webber',
+        content: 'reacted to your recent post',
+        date: '1m ago',
+        unread: true,
+        type : 'link',
+        meta:{
+            linkType: 'post',
+            linkText: 'My first tournament today!'
+        }
+    }
+]
+
+
 // Get DOM elements
 const markAllasReadElement = document.querySelector('.header__link');
 const unreadCountElement = document.querySelector('.unread-count');
@@ -17,7 +51,7 @@ function markAllasRead(){
     //get all unread notifications
     const unreadNotifications = document.querySelectorAll('.notification--unread');
     unreadNotifications.forEach(notification=>{
-        notification.classList.remove('notification--unread');
+       
        
     });
    
@@ -28,9 +62,10 @@ function markAllasRead(){
     unreadBubble.forEach(notification=>{
         notification.classList.remove('unread-bubble');
     });
-    markAllasReadElement.textContent ="Mark all as unread";
-   
+    markAllasReadElement.textContent ="Mark all as unread"; 
 }
+
+
 
 // reset unread count
 function resetUnreadCount(countNotification){
@@ -42,4 +77,28 @@ function resetUnreadCount(countNotification){
 
     }
     
+}
+
+// Compose notification DOM element
+function composeNotification(notification){
+    // notification wrapper 
+    const notificationElement = document.createElement('article');
+    notificationElement.classList.add('notification');
+
+    // avatar
+    const avatarElement = document.createElement('img');
+    avatarElement.classList.add('avatar');
+    avatarElement.alt = notification.name;
+    avatarElement.src = notification.avatar;
+
+    // title
+    const titleElement=document.createElement('h2');
+    h2.classList.add('notification__title');
+    let titleContent = `<strong>${notification.name}</strong> ${notification.content}`;
+
+
+    // Control notification states
+    if(notification.unread){
+        notificationElement.classList.add('notification--unread');
+    }
 }
